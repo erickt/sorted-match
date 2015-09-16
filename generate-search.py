bf02b22af7ef6b454aa0c7303f1998d9dfb20d9a
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2015-09-15 21:52:21 -0700

    Add in tries

--- generate-search.py
+++ generate-search.py
@@ -1,11 +1,12 @@
 import textwrap
 import pprint
 import sys
+import itertools
 
-prefix = sys.argv[1]
+prefixes = sys.argv[1].split(',')
 count = int(sys.argv[2])
 
-haystack = [prefix + c for c in [
+haystack_template = [
     'a',
     'b',
     'c',
@@ -32,181 +33,268 @@ haystack = [prefix + c for c in [
     'x',
     'y',
     'z',
-]][:count]
+]
 
-print """\
-use std::cmp::Ordering;
+haystack = []
 
-pub static HAYSTACK: &'static [&'static str] = &[%s];
-""" % ','.join('"%s"' % hay for hay in haystack)
+for i, c in enumerate(itertools.cycle(haystack_template)):
+  if i >= count:
+    break
 
-print """\n
-#[inline]
-fn cmp_slice(a: &str, b: &str) -> Ordering {
-    // NOTE: In theory n should be libc::size_t and not usize, but libc is not available here
-    #[allow(improper_ctypes)]
-    extern { fn memcmp(s1: *const i8, s2: *const i8, n: usize) -> i32; }
-    unsafe {
-        let cmp = memcmp(a.as_ptr() as *const i8, b.as_ptr() as *const i8, a.len());
-        if cmp == 0 {
-            a.len().cmp(&b.len())
-        } else if cmp < 0 {
-            Ordering::Less
-        } else {
-            Ordering::Greater
-        }
+  prefix = prefixes[i % len(prefixes)]
+  haystack.append(prefix + c)
 
-    }
-}
+def generate_header():
+  print textwrap.dedent("""
+  use std::cmp::Ordering;
 
-/*
-//#[no_mangle]
-//#[inline(never)]
-fn cmp_slice(a: &str, b: &str) -> Ordering {
-    for (a_byte, b_byte) in a.bytes().zip(b.bytes()) {
-        if a_byte < b_byte {
-            return Ordering::Less;
-        } else if a_byte > b_byte {
-            return Ordering::Greater;
-        }
-    }
+  pub static HAYSTACK: &'static [&'static str] = &[%s];
+  """ % ','.join('"%s"' % hay for hay in haystack))
 
-    if a.len() < b.len() {
-        return Ordering::Less;
-    } else if a.len() > b.len() {
-        return Ordering::Greater;
-    } else {
-        return Ordering::Equal;
-    }
-}
-*/
+  print textwrap.dedent("""
 
-//#[no_mangle]
-#[inline]
-fn eq_slice(a: &str, b: &str) -> bool {
-    // NOTE: In theory n should be libc::size_t and not usize, but libc is not available here
-    #[allow(improper_ctypes)]
-    extern { fn memcmp(s1: *const i8, s2: *const i8, n: usize) -> i32; }
-    a.len() == b.len() && unsafe {
-        memcmp(a.as_ptr() as *const i8,
-               b.as_ptr() as *const i8,
-               a.len()) == 0
-    }
-}
-"""
+  #[inline]
+  fn cmp_slice(a: &str, b: &str) -> Ordering {
+      // NOTE: In theory n should be libc::size_t and not usize, but libc is not available here
+      #[allow(improper_ctypes)]
+      extern { fn memcmp(s1: *const i8, s2: *const i8, n: usize) -> i32; }
+      unsafe {
+          let cmp = memcmp(a.as_ptr() as *const i8, b.as_ptr() as *const i8, a.len());
+          if cmp == 0 {
+              a.len().cmp(&b.len())
+          } else if cmp < 0 {
+              Ordering::Less
+          } else {
+              Ordering::Greater
+          }
+
+      }
+  }
+  """)
 
 # ------------------------------------------------------------------------------
 
-print """\
-#[no_mangle]
-#[inline(never)]
-pub fn match_search(needle: &str) -> usize {
-    match needle {"""
+def generate_match():
+  print textwrap.dedent("""
+  #[no_mangle]
+  #[inline(never)]
+  pub fn match_search(needle: &str) -> usize {
+      match needle {""")
 
-for i, hay in enumerate(haystack):
-  print '        "%s" => %s,' % (hay, i)
+  for i, hay in enumerate(haystack):
+    print '        "%s" => %s,' % (hay, i)
 
-print """\
-        _ => %s
-    }
-}""" % (i + 1)
-print
-
-# ------------------------------------------------------------------------------
-
-print """\
-//#[no_mangle]
-#[inline(never)]
-pub fn linear_std_search(needle: &str) -> usize {"""
-
-first = True
-for i, hay in enumerate(haystack):
-  if first:
-    first = False
-    print '    if needle == "%s" { %s }' % (hay, i)
-  else:
-    print '    else if needle == "%s" { %s }' % (hay, i)
-
-print """\
-    else { %s }
-}""" % (i + 1)
-print
+  print '        _ => %s' % (i + 1)
+  print '    }'
+  print '}'
 
 # ------------------------------------------------------------------------------
 
-print """\
-//#[no_mangle]
-#[inline(never)]
-pub fn linear_memcmp_search(needle: &str) -> usize {"""
+def generate_linear():
+  print textwrap.dedent("""
+  //#[no_mangle]
+  #[inline(never)]
+  pub fn linear_search(needle: &str) -> usize {""")
 
-first = True
-for i, hay in enumerate(haystack):
-  if first:
-    first = False
-    print '    if eq_slice(needle, "%s") { %s }' % (hay, i)
-  else:
-    print '    else if eq_slice(needle, "%s") { %s }' % (hay, i)
+  first = True
+  for i, hay in enumerate(haystack):
+    if first:
+      first = False
+      print '    if needle == "%s" { %s }' % (hay, i)
+    else:
+      print '    else if needle == "%s" { %s }' % (hay, i)
 
-print """\
-    else { %s }
-}""" % (i + 1)
-print
+  print '    else { %s }' % (i + 1)
+  print '}'
 
 # ------------------------------------------------------------------------------
 
-ordering = []
-
-def walk(fn, start, end, indent=0):
+def walk_binary(fn, start, end, indent=0):
   if start == end:
     return ""
 
   index = (end + start) // 2
   spaces = ' ' * indent
 
-  less = walk(fn, start, index, indent=indent + 8)
+  less = walk_binary(fn, start, index, indent=indent + 8)
   if less:
     less = '{\n%s\n%s}' % (less, spaces + ' ' * 4)
   else:
     less = '{ }'
 
-  greater = walk(fn, index + 1, end, indent=indent + 8)
+  greater = walk_binary(fn, index + 1, end, indent=indent + 8)
   if greater:
     greater = '{\n%s\n%s}' % (greater, spaces + ' ' * 4)
   else:
     greater = '{ }'
 
-  return """\
-%smatch %s(needle, "%s") {
-%s    Ordering::Less => %s
-%s    Ordering::Equal => { return %s; }
-%s    Ordering::Greater => %s
-%s}""" % (
-      spaces,
-      fn,
-      haystack[index],
-      spaces,
-      less,
-      spaces,
-      index,
-      spaces,
-      greater,
-      spaces,
-  )
+  lines = [
+    'match %s(needle, "%s") {' % (fn, haystack[index]),
+    '    Ordering::Less => %s' % less,
+    '    Ordering::Equal => { return %s; }' % index,
+    '    Ordering::Greater => %s' % greater,
+    '}',
+  ]
 
-print """\
-//#[no_mangle]
-#[inline(never)]
-pub fn binary_std_search(needle: &str) -> usize {"""
-print walk('str::cmp', 0, len(haystack), 4)
-print """\
-    %s
-}""" % len(haystack)
+  return '\n'.join('%s%s' % (spaces, line) for line in lines)
 
-print """\
-//#[no_mangle]
-#[inline(never)]
-pub fn binary_memcmp_search(needle: &str) -> usize {"""
-print walk('cmp_slice', 0, len(haystack), 4)
-print """\
-    %s
-}""" % len(haystack)
+def generate_binary():
+  ordering = []
+
+  print textwrap.dedent("""
+  //#[no_mangle]
+  #[inline(never)]
+  pub fn binary_search(needle: &str) -> usize {""")
+  print walk_binary('cmp_slice', 0, len(haystack), 4)
+  print '    %s' % len(haystack)
+  print '}'
+
+# ------------------------------------------------------------------------------
+
+class Trie(object):
+  def __init__(self):
+    self.root = TrieNode()
+
+  def insert(self, key, value):
+    chars = list(key)
+    chars.reverse()
+    self.root.insert(chars, value)
+
+  def walk(self):
+    return self.root.walk(4)
+
+  def print_trie(self):
+    self.root.print_trie(0)
+
+  def compress(self):
+    self.root.compress(0)
+
+class TrieNode(object):
+  def __init__(self):
+    self.value = None
+    self.children = {}
+
+  def insert(self, chars, value):
+    assert len(chars) > 0
+
+    char = chars.pop()
+
+    try:
+      node = self.children[char]
+    except KeyError:
+      node = self.children[char] = TrieNode()
+
+    if len(chars) == 0:
+      node.value = value
+    else:
+      node.insert(chars, value)
+
+  def walk(self, depth):
+    assert self.children
+
+    length = len(next(self.children.iterkeys()))
+
+    lines = []
+
+    if length == 1:
+      lines.extend([
+          '%slet (prefix, needle) = (needle[0], &needle[1..]);' % (' ' * depth,),
+      ])
+    else:
+      lines.append(
+          '%slet (prefix, needle) = needle.split_at(%s);' % (' ' * depth, length)
+      )
+
+    lines.append(
+        '%smatch prefix {' % (' ' * depth,)
+    )
+
+    for key, child in self.children.iteritems():
+      s = [' ' * (depth + 4)]
+
+      if len(key) == 1:
+        s.append("b'%s'" % key)
+      else:
+        s.append('b"%s"' % key)
+
+      s.append(' => {')
+
+      lines.append(''.join(s))
+
+      if child.value is not None:
+        lines.append('%sif needle.is_empty() {' % (' ' * (depth + 8),))
+        lines.append('%sreturn %s;' % (' ' * (depth + 12), child.value))
+        lines.append('%s}' % (' ' * (depth + 8),))
+
+        if child.children:
+          lines.append(child.walk(depth + 8))
+      else:
+        lines.append(child.walk(depth + 8))
+
+      lines.append('%s}' % (' ' * (depth + 4),))
+
+    lines.append('%s_ => { }' % (' ' * (depth + 4),))
+    lines.append('%s}' % (' ' * depth,))
+
+    return '\n'.join(lines)
+
+  def print_trie(self, depth):
+    for i, (key, node) in enumerate(sorted(self.children.iteritems())):
+      if i == 0:
+        prefix = '+'
+      else:
+        prefix = '|'
+      print ' ' * depth, prefix, key,
+
+      if node.value is not None:
+        print '=>', node.value
+      else:
+        print
+
+      node.print_trie(depth + 2)
+
+  def compress(self, depth):
+    #print 'compressing:', ' ' * depth, self.key, self.children.keys()
+
+    while len(self.children) == 1:
+      key, node = self.children.popitem()
+      if len(node.children) == 1:
+        for k, n in node.children.iteritems():
+          self.children[key + k] = n
+      else:
+        self.children[key] = node
+        break
+
+    for node in self.children.itervalues():
+      node.compress(depth + 2)
+
+
+def generate_trie():
+  trie = Trie()
+
+  for i, hay in enumerate(haystack):
+    trie.insert(hay, i)
+
+  trie.compress()
+
+  print textwrap.dedent("""
+  #[inline(never)]
+  pub fn trie_search(needle: &str) -> usize {
+      let needle = needle.as_bytes();
+  """)
+
+  print trie.walk()
+  print '    %s' % len(haystack)
+  print '}'
+
+# ------------------------------------------------------------------------------
+
+def main():
+  generate_header()
+  generate_match()
+  generate_linear()
+  generate_binary()
+  generate_trie()
+
+if __name__ == '__main__':
+  sys.exit(main())
